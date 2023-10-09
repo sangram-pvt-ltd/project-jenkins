319fbc1df96815fa36bd6681cca130617de1117a
Sangramshinde97 <108196042+Sangramshinde97@users.noreply.github.com>
2023-10-09 19:37:31 +0530

    Update App.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello, sangram,akshay,vinesh and React,abc,pqr,helloword,bye!</h1>
+      <h1>Hello, sangram,akshay,vinesh and React,abc,pqr,helloword,bye,akshay!</h1>
     </div>
   );
 }
