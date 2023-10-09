b2eff6512717ad0e574e6ab20a49e02ce2f32e86
Sangramshinde97 <108196042+Sangramshinde97@users.noreply.github.com>
2023-10-09 19:39:32 +0530

    Update index.ts

--- argo-learning-foo-app/index.ts
+++ argo-learning-foo-app/index.ts
@@ -13,7 +13,7 @@ const server = createServer((request: IncomingMessage, response: ServerResponse)
           process.exit(1);
         }
         response.setHeader("Connection", "close");
-        response.end(`Hello world ${counter} from ${hostname()}, from app ${process.env.APP_NAME}. Version - 1.0.1`);
+        response.end(`Hello world akshay ${counter} from ${hostname()}, from app ${process.env.APP_NAME}. Version - 1.0.1`);
       }
 
       break;
